06f6e1ea2631bab8292a872cac7b50d75d4e1709
curzonj <11769+curzonj@users.noreply.github.com>
2019-12-12 02:33:05 +0000

    Deploying to gh-pages from master 7010648f3f106593a28ae8f6fc5fcf8ad9c80ae3

---
+++ precache-manifest.52987fcc04b3d9e9b922a74d5ce1a605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a7dd3f705833568fc1c32f02b86329",
+    "url": "/brain/index.html"
+  },
+  {
+    "revision": "c7c4a190ce7581440bc6",
+    "url": "/brain/static/css/main.cff09f10.chunk.css"
+  },
+  {
+    "revision": "38d04b135b3a194c2389",
+    "url": "/brain/static/js/2.6b575318.chunk.js"
+  },
+  {
+    "revision": "c7c4a190ce7581440bc6",
+    "url": "/brain/static/js/main.2a5e7af0.chunk.js"
+  },
+  {
+    "revision": "5c772861941659ba23ed",
+    "url": "/brain/static/js/runtime~main.66a97e6c.js"
+  }
+]);
